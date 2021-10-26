045adec1702a39c4d6b7e937f4570051a25fb25e
dotroo <vinick96@gmail.com>
2021-10-26 13:09:06 +0300

    added check for HTTP method

--- src/configs/dbconfig.php
+++ src/configs/dbconfig.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'DBHOST'    => 'localhost',
-    'DBNAME'    => 'links',
-    'DBCHARSET' => 'utf8',
-    'DBLOGIN'   => 'script',
-    'DBPASS'    => 'phppass'
+    'DBHOST'    => '',
+    'DBNAME'    => '',
+    'DBCHARSET' => '',
+    'DBLOGIN'   => '',
+    'DBPASS'    => ''
 ];
